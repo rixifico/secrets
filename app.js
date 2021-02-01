9a984f816c2896f001815ecd2e51d56e9b5d059d
Ricardo Perez martinez <ricardoperezmartinez@MacBook-Pro-de-Ricardo.local>
2021-02-01 20:30:22 +0100

    env files added

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 //jshint esversion:6
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
@@ -32,8 +33,7 @@ const userSchema = new mongoose.Schema({
 
 //ENCRYPT PASSWORD
 
-const secret = "Thisisourlittlesecret."
-userSchema.plugin(encrypt,{secret:secret, encryptedFields:["password"]});
+userSchema.plugin(encrypt,{secret:process.env.SECRET, encryptedFields:["password"]});
 
 
 //CREATE MODEL
