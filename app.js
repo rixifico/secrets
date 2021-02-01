7c641288a7ffe84607968c9c6fdef5ed5c7171c7
Ricardo Perez martinez <ricardoperezmartinez@MacBook-Pro-de-Ricardo.local>
2021-02-01 17:56:33 +0100

    level 2

--- app.js
+++ app.js
@@ -1,65 +1,84 @@
 //jshint esversion:6
 const express = require("express");
 const bodyParser = require("body-parser");
-const ejs = require ("ejs");
+const ejs = require("ejs");
 const mongoose = require("mongoose")
+const encrypt = require("mongoose-encryption")
 
 const app = express();
 
 
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 app.set("view engine", "ejs");
 
 app.use(express.static("public"));
 
-mongoose.connect('mongodb://localhost:27017/userDB', {useNewUrlParser: true, useUnifiedTopology: true});
 
-const userSchema = {
+//CREATE DATABASE
+mongoose.connect('mongodb://localhost:27017/userDB', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+
+//CREATE SCHEMA
+const userSchema = new mongoose.Schema({
   email: String,
   password: String
-};
+});
 
+//ENCRYPT PASSWORD
+
+const secret = "Thisisourlittlesecret."
+userSchema.plugin(encrypt,{secret:secret, encryptedFields:["password"]});
+
+
+//CREATE MODEL
 const User = new mongoose.model("User", userSchema);
 
 
-app.get("/", function(req, res){
+app.get("/", function(req, res) {
   res.render("home");
 });
 
-app.get("/login", function(req, res){
+app.get("/login", function(req, res) {
   res.render("login");
 });
 
-app.get("/register", function(req, res){
+app.get("/register", function(req, res) {
   res.render("register");
 });
 
-app.post("/register", function(req,res){
+app.post("/register", function(req, res) {
   const newUser = new User({
     email: req.body.username,
     password: req.body.password
   });
-  newUser.save(function(err){
-    if (err){
+  newUser.save(function(err) {
+    if (err) {
       console.log(err);
-    }else{
+    } else {
       res.render("secrets");
     }
   })
 })
 
-app.post("/login", function(req,res){
+app.post("/login", function(req, res) {
   const username = req.body.username
   const password = req.body.password
 
-  User.findOne({email:username},function(err, foundUser){
-    if(!foundUser){
+  User.findOne({
+    email: username
+  }, function(err, foundUser) {
+    if (!foundUser) {
       res.send("wrong username")
-    }else{
-      if(foundUser.password === password){
+    } else {
+      if (foundUser.password === password) {
         res.render("secrets");
-      }else{
+      } else {
         res.send("Wrong username or password")
       }
     }
@@ -67,6 +86,6 @@ app.post("/login", function(req,res){
 })
 
 
-app.listen(3000, function(){
+app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
